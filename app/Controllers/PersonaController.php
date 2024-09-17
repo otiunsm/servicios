3a1c66ce43479c8f40695916a4602940d629499e
Andre <andre251295>
2024-09-17 08:50:35 -0500

    Creacion del controlador Persona

---
+++ app/Controllers/PersonaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PersonaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
