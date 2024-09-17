6bcc2466646fa78e76bea7cb1a617e9694e11285
clauadri06 <cr0263244@gmail.com>
2024-09-17 09:23:23 -0500

    modificacion del controller persona

--- app/Controllers/PersonaController.php
+++ app/Controllers/PersonaController.php
@@ -3,11 +3,20 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\PersonaModel; // Asegúrate de tener el modelo PersonaModel
 
 class PersonaController extends BaseController
 {
     public function index()
     {
-        //
+        // Instanciamos el modelo PersonaModel
+        $personaModel = new PersonaModel();
+        
+        // Obtenemos todas las personas de la base de datos
+        $personas = $personaModel->findAll();
+        
+        // Pasamos los datos a la vista
+        return view('persona/list', ['personas' => $personas]);
     }
 }
+
